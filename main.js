d9c92cd927f06a4c6cdecaf4e7a14a2007f7f209
MarikaMorittu <marikaegio@hotmail.it>
2022-05-31 22:44:18 +0200

    dynamic code

--- main.js
+++ main.js
@@ -1,46 +1,38 @@
+// dichiarazione variabili
 let remove = document.getElementById("minus");
 let add = document.getElementById("plus");
 let number = document.getElementById("number");
-
+// plus and minus
 remove.addEventListener("click", () => {
-  number.value--;
+  if (number.value > 0) {
+    number.value--;
+  }
 });
 
 add.addEventListener("click", () => {
   number.value++;
 });
+// array
+const arrayImg = [
+  "./img/image-product-1-thumbnail.jpg",
+  "./img/image-product-2-thumbnail.jpg",
+  "./img/image-product-3-thumbnail.jpg",
+  "./img/image-product-4-thumbnail.jpg",
+];
+// dichiarazione variabili
+let containerImage = document.querySelector(".container__thumbnail__image");
 
-let img1 = document.getElementById("img1");
-let img2 = document.getElementById("img2");
-let img3 = document.getElementById("img3");
-let img4 = document.getElementById("img4");
+let bigImage = document.querySelector(".img__product");
 
-let imgThumbnail1 = document.getElementById("img-th1");
-let imgThumbnail2 = document.getElementById("img-th2");
-let imgThumbnail3 = document.getElementById("img-th3");
-let imgThumbnail4 = document.getElementById("img-th4");
-
-imgThumbnail1.addEventListener("click", () => {
-  document
-    .querySelector(".img-shose:not(.display-none)")
-    .classList.add("display-none");
-  img1.classList.remove("display-none");
-});
-imgThumbnail2.addEventListener("click", () => {
-  document
-    .querySelector(".img-shose:not(.display-none)")
-    .classList.add("display-none");
-  img2.classList.remove("display-none");
-});
-imgThumbnail3.addEventListener("click", () => {
-  document
-    .querySelector(".img-shose:not(.display-none)")
-    .classList.add("display-none");
-  img3.classList.remove("display-none");
-});
-imgThumbnail4.addEventListener("click", () => {
-  document
-    .querySelector(".img-shose:not(.display-none)")
-    .classList.add("display-none");
-  img4.classList.remove("display-none");
+for (i = 0; i < arrayImg.length; i++) {
+  let img = document.createElement("img");
+  img.src = arrayImg[i];
+  containerImage.append(img);
+  img.classList.add("img-thumbnail");
+}
+const img = document.querySelectorAll(".img-thumbnail");
+img.forEach((e) => {
+  e.addEventListener("click", () => {
+    bigImage.src = e.src.replace("-thumbnail", "");
+  });
 });
